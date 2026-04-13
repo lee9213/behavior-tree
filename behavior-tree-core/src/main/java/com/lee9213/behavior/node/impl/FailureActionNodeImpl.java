a8a60df33913adb45bb2629b5a156a7a15ee833b
lee9213 <lee9213@163.com>
2026-04-13 22:46:34 +0800

    feat: modify action node implementations to extend AbstractActionNode

--- behavior-tree-core/src/main/java/com/lee9213/behavior/node/impl/FailureActionNodeImpl.java
+++ behavior-tree-core/src/main/java/com/lee9213/behavior/node/impl/FailureActionNodeImpl.java
@@ -2,15 +2,20 @@ package com.lee9213.behavior.node.impl;
 
 import com.lee9213.behavior.BaseContext;
 import com.lee9213.behavior.NodeResult;
-import com.lee9213.behavior.node.IActionNode;
+import com.lee9213.behavior.RetryPolicy;
 
 /**
  * @author lee9213@163.com
  * @date 2024/5/31 09:44
  */
-public final class FailureActionNodeImpl<Result extends NodeResult,Context extends BaseContext> implements IActionNode<Result, Context> {
+public final class FailureActionNodeImpl<Result extends NodeResult,Context extends BaseContext> extends AbstractActionNode<Result, Context> {
+
+    public FailureActionNodeImpl(String nodeName) {
+        super(nodeName, false, null);
+    }
+
     @Override
-    public Result execute(Context context) {
+    protected Result doExecute(Context context) {
         return (Result) NodeResult.FAILURE;
     }
 }
--- behavior-tree-core/src/main/java/com/lee9213/behavior/node/impl/SuccessActionNodeImpl.java
+++ behavior-tree-core/src/main/java/com/lee9213/behavior/node/impl/SuccessActionNodeImpl.java
@@ -2,16 +2,20 @@ package com.lee9213.behavior.node.impl;
 
 import com.lee9213.behavior.BaseContext;
 import com.lee9213.behavior.NodeResult;
-import com.lee9213.behavior.node.IActionNode;
+import com.lee9213.behavior.RetryPolicy;
 
 /**
  * @author lee9213@163.com
  * @date 2024/5/31 09:43
  */
-public final class SuccessActionNodeImpl<Result extends NodeResult,Context extends BaseContext> implements IActionNode<Result, Context> {
+public final class SuccessActionNodeImpl<Result extends NodeResult,Context extends BaseContext> extends AbstractActionNode<Result, Context> {
+
+    public SuccessActionNodeImpl(String nodeName) {
+        super(nodeName, false, null);
+    }
 
     @Override
-    public Result execute(Context context) {
+    protected Result doExecute(Context context) {
         return (Result) NodeResult.SUCCESS;
     }
 }
